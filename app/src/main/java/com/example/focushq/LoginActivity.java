e6a6b8e06a13090611fd80f476037beba0bdf52b
bmalv <bianca.alvarado@utexas.edu>
2021-07-13 14:13:30 -0500

    created bottom navigation view and can view different empty fragments for each tab.

--- app/src/main/java/com/example/focushq/LoginActivity.java
+++ app/src/main/java/com/example/focushq/LoginActivity.java
@@ -28,6 +28,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //checking if user is already signed in
+        if(ParseUser.getCurrentUser() != null){
+            //user is already signed in show main activity
+            goMainActivity();
+        }
+
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
--- app/src/main/java/com/example/focushq/MainActivity.java
+++ app/src/main/java/com/example/focushq/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.focushq;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.focushq.fragments.ComposeFragment;
+import com.example.focushq.fragments.PostsFragment;
+import com.example.focushq.fragments.ProfileFragment;
+import com.example.focushq.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        // do something here
+                        Log.d(TAG, "home!");
+                        fragment = new PostsFragment();
+                        break;
+                    case R.id.action_compose:
+                        // do something here
+                        Log.d(TAG, "compose!");
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_search:
+                        // do something here
+                        Log.d(TAG, "search!");
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.action_profile:
+                        Log.d(TAG, "profile!");
+                        fragment = new ProfileFragment();
+                        // do something here
+                        break;
+                    default: break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+               return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
