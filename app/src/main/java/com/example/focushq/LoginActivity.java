8978ad0747539b0516e4438f578fc0b578621719
bmalv <bianca.alvarado@utexas.edu>
2021-07-13 11:44:51 -0500

    user can sign up and log in!

---
+++ app/src/main/java/com/example/focushq/LoginActivity.java
@@ -0,0 +1,104 @@
+package com.example.focushq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username,password);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick sign up button");
+                ParseUser user = new ParseUser();
+                //sets core user properties
+                user.setUsername(etUsername.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                Log.d(TAG, "username: " + username + " password: " + password);
+                //invoke sign up in background
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // Hooray! Let them use the app now.
+                            Log.d(TAG,"Sign up success!");
+                            goMainActivity();
+                            Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Sign up didn't succeed. Look at the ParseException
+                            // to figure out what went wrong
+                            Log.i(TAG,"error creating new user " + e);
+                            return;
+                        }
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    //TODO: better error handling
+                    Log.e(TAG, "issue with login", e);
+                    Toast.makeText(LoginActivity.this,"Issue with login!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                /*navigate to the main activity if the user has signed in properly */
+                Log.i(TAG,"Login Success!");
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //function will start an intent to the main activity
+    public void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
--- app/src/main/java/com/example/focushq/ParseApplication.java
+++ app/src/main/java/com/example/focushq/ParseApplication.java
@@ -3,20 +3,35 @@ package com.example.focushq;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // set applicationId, and server server based on the values in the back4app settings.
-        // any network interceptors must be added with the Configuration Builder given this syntax
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See https://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("nCo2aQy2KujDF7vDA4L4yKNOHK4mOEI1Ft3BBa8d") // should correspond to Application Id env variable
-                .clientKey("vAfAE9MhU6xuzKk2tebdnkU9JteRQnvht86yFbpP")  // should correspond to Client key env variable
-                .server("https://parseapi.back4app.com").build());
+                .applicationId("nCo2aQy2KujDF7vDA4L4yKNOHK4mOEI1Ft3BBa8d")
+                .clientKey("vAfAE9MhU6xuzKk2tebdnkU9JteRQnvht86yFbpP")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+
+        // New test creation of object below
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 }
