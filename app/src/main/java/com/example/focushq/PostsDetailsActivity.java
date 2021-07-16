75a4faee2c7de58404cc9f4dc8af9ef99fa7d4ef
bmalv <bianca.alvarado@utexas.edu>
2021-07-16 10:28:46 -0500

    user can exit from post details back to the main feed screen!

--- app/src/main/java/com/example/focushq/PostsDetailsActivity.java
+++ app/src/main/java/com/example/focushq/PostsDetailsActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
+import com.parse.ParseUser;
 
 import org.parceler.Parcels;
 
@@ -56,5 +59,30 @@ public class PostsDetailsActivity extends AppCompatActivity {
                     .into(ivImage);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar.
+        Log.d("PostsDetailsActivity","in inflating menu");
+        getMenuInflater().inflate(R.menu.menu_back_button, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_back:
+                // Handle this selection
+                ParseUser.logOut();
+                Log.i("PostsDetailsActivity", "user taken back to home screen");
+                //go back to home screen
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
